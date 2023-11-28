5ae12a165e7afcbbf080e3dc582c2033729bd063
Nicodev61 <nicolas.chevreau@sfr.fr>
2023-11-28 15:04:17 +0100

    création fonction getCollection et addObject

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,30 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
 const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 
-const nombreAléatoire = ()=>{
-
-    let rand = Math.floor(Math.random()*100).toString();
-
-    return rand;
-}
-
-
- exports.apir = onRequest((request, response) => {
-   logger.info("Hello logs!", {structuredData: true});
-   response.send(nombreAléatoire());
+const {getFirestore} = require("firebase-admin/firestore");
+const {initializeApp} = require("firebase-admin/app");
+initializeApp();
+exports.getCollection = onRequest(async (req, res) => {
+  // Get a reference to the "cartes" collection
+  const cartesRef = getFirestore().collection("cartes");
+  // Get all documents from the "cartes" collection
+  const snapshot = await cartesRef.get();
+  // Map the documents to an array of data objects
+  const messages = snapshot.docs.map(doc => doc.data());
+  // Send the data back as a response
+  res.json({ messages });
  });
+
+ exports.addObject = onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.body;
+  console.log(original)
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await getFirestore()
+      .collection(original.collection)
+      .add({symbole: original.symbole,
+      couleur:original.couleur,
+    figure:original.figure});
+  // Send back a message that we've successfully written the message
+  res.json({result: `Message with ID: ${writeResult.id} added.`});
+});
